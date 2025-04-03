a3769c64703746ba4588003c3f94a369b854d41a
EnzoFamiliar-Marais <enzo.familiar-marais@etu.univ-orleans.fr>
2025-04-03 09:43:43 +0200

    footer

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -9,7 +9,7 @@ class Footer extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       color: Colors.grey[200],
       child: const Center(
-        child: Text('© 2023 IUTables’O - Tous droits réservés'),
+        child: Text('© 2025 IUTables’O - Jean-Marc Jorite / Enzo Familiar-Marais / Romain Lima / Mohammed-Amine Yahyaoui '),
       ),
     );
   }
